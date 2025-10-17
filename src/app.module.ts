5d69a0cdede4648a098ed9862d9a409d5e76662a
MatiOrtegga <groundwtf911@gmail.com>
2025-10-17 07:42:33 -0300

    Adding swagger, and create the structure folders.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
